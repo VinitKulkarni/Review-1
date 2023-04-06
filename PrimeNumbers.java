8023ee927f234648ba88fc12f9b569a26c5585b0
vinit <vinitkkulkarni@gmail.com>
2023-04-06 07:22:16 +0530

    Refactor: comment line

--- PrimeNumbers.java
+++ PrimeNumbers.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 //Just like the Perfect number, the Prime number is also a special type of number. When
 //the number is divided greater than 1 and divided by 1 or itself is referred to as the Prime
 //number. 0 and 1 are not counted as prime numbers. All the even numbers can be
-//divided by 2, so 2 is the only even prime minister.
+//divided by 2, so 2 is the only even prime number.
 public class PrimeNumbers {
 
     static boolean checkForPrime(int data){
